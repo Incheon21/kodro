f64908e24f58f9a13a779499640d92a0f7832632
Incheon21 <alvinchrisantausa@gmail.com>
2026-03-23 20:56:14 +0700

    updated kodro 2026

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Link from "next/link"
+import { FaInstagram } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa6";
 
 const Footer = () => {
   return (
@@ -9,12 +12,20 @@ const Footer = () => {
       }}
     >
       <div className="max-w-7xl mx-auto px-6 py-8">
-        <div className="flex flex-col md:flex-row justify-center items-center">
+        <div className="flex flex-col md:flex-row justify-between items-center">
           <div
             className="text-sm mb-4 md:mb-0"
             style={{ color: "var(--foreground)" }}
           >
-            © 2025 Kodro. All rights reserved.
+            © 2026 Kodro. All rights reserved.
+          </div>
+          <div className="flex flex-row gap-2 text-xl" style={{ color: "var(--foreground)" }}>
+            <Link href="https://www.instagram.com/kodro.dev/  " target="_blank" rel="noopener noreferrer">
+              <FaInstagram className="hover:scale-[1.05]"/>
+            </Link>
+            <Link href="https://wa.me/6285121096113" target="_blank" rel="noopener noreferrer">
+              <FaWhatsapp className="hover:scale-[1.05]"/>
+            </Link>
           </div>
         </div>
       </div>
